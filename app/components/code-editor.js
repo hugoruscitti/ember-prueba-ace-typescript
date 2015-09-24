2054e858ab4a514c7b873a237478795115a5df15
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-09-24 16:33:53 -0300

    arreglos menores.

--- app/components/code-editor.js
+++ app/components/code-editor.js
@@ -9,15 +9,18 @@ export default Ember.Component.extend({
     this.editor.getSession().setValue(this.get('value'));
     this.sendAction('aceInit', this.editor);
 
-    this.editor.on('change', function(){
+    this.editor.on('change', () => {
       this.set('value', this.editor.getSession().getValue());
-    }.bind(this));
+    });
   },
 
   valueChanged: function () {
-    if (!this.get('value'))
+    if (!this.get('value')) {
       this.editor.getSession().setValue('');
-    else if (this.editor.getSession().getValue() !== this.get('value'))
-      this.editor.getSession().setValue(this.get('value'));
+    } else {
+      if (this.editor.getSession().getValue() !== this.get('value')) {
+        this.editor.getSession().setValue(this.get('value'));
+      }
+    }
   }.observes('value')
 });
